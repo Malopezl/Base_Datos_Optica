8d5d1a59ac842a4366e3dd4292e4683305464efa
Sharon <sharonubi98@gmail.com>
2019-03-11 12:30:29 -0600

    Subiendo views
    Views de correo, compra y proveedores

---
+++ views/proveedores/_search.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\ProveedoresSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="proveedores-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'ID') ?>
+
+    <?= $form->field($model, 'Direccion') ?>
+
+    <?= $form->field($model, 'Nombre') ?>
+
+    <?= $form->field($model, 'NIT') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
