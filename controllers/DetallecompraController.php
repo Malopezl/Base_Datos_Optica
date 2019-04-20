216547e97f5d9adf19085ba272e8d39092810210
marcos <sierramarcos146@gmail.com>
2019-04-19 19:05:32 -0600

    subiendo controllers

--- controllers/DetallecompraController.php
+++ controllers/DetallecompraController.php
@@ -5,6 +5,9 @@ namespace app\controllers;
 use Yii;
 use app\models\Detallecompra;
 use app\models\DetallecompraSearch;
+use app\models\Lente;
+use app\models\Accesorios;
+use app\models\Aro;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -62,18 +65,114 @@ class DetallecompraController extends Controller
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
-    public function actionCreate()
+    public function actionCreate($id, $ida)
     {
+       
         $model = new Detallecompra();
+        if($ida!=0)
+        {
+            $model->ID_Accesorio=$ida;
+        }
+        $model->ID_Compra = $id;
+        $accesorioss = [];
+        $tmp = Accesorios::find()->all();
+        foreach($tmp as $accesorio){
+            $accesorioss[$accesorio->idAccesorio]="Nombre: ".$accesorio->Nombre.";  Descripcion: ".$accesorio->Descripcion.";  Existencia: ".$accesorio->Existencia;
+
+        }
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->ID]);
+            return $this->redirect(['compra/creates', 'id' => $id]);
         }
 
         return $this->render('create', [
             'model' => $model,
+            'accesorioss'=>$accesorioss,
+            'id'=>$id,
         ]);
     }
+    public function actionCreatelt($id, $idlen)
+    {
+
+        $model = new Detallecompra();
+        if($idlen != 0)
+        {
+            $model->Lente_idLente=$idlen;
+        }
+        $model->ID_Compra = $id;
+        $lentes =[];
+        $tmp = Lente::find()->all();
+        foreach($tmp as $len){
+            if($len->Terminado==1)
+            {
+
+                $lentes[$len->idLente]="Material:".$len->Material."; Tipo:".$len->Tipo."Precio Venta: ".$len->Precio_Venta."; Graduacion: ".$len->graduacion_base."; Excedente: ".$len->Graduacion_Ecxedente;
+            }
+        }
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['compra/creates', 'id' => $id]);
+        }
+
+        return $this->render('createlt', [
+            'model' => $model,
+            'lentes' => $lentes,
+            'id'=>$id,
+        ]);
+    }
+
+    public function actionCreatels($id, $idlen)
+    {
+        $model = new Detallecompra();
+        if($idlen != 0)
+        {
+            $model->Lente_idLente=$idlen;
+        }
+        $model->ID_Compra = $id;
+        $lentes =[];
+        $tmp = Lente::find()->all();
+        foreach($tmp as $len){
+            if($len->Terminado == 0)
+            {
+
+                $lentes[$len->idLente]="Material:".$len->Material."; Tipo:".$len->Tipo."Precio Venta: ".$len->Precio_Venta."; Graduacion: ".$len->graduacion_base."; Excedente: ".$len->Graduacion_Ecxedente;
+            }
+        }
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['compra/creates', 'id' => $id]);
+        }
+
+        return $this->render('createls', [
+            'model' => $model,
+            'lentes' => $lentes,
+            'id'=>$id,
+        ]);
+    }
+
+    public function actionCreatea($id, $ida)
+    {
+        $model = new Detallecompra();
+        if($ida != 0)
+        {
+            $model->ID_Aro = $ida;
+        }
+        $model->ID_Compra = $id;
+        $aros= [];
+        $tmp1 = Aro::find()->all();
+        foreach($tmp1 as $ar){
+            $aros[$ar->idAro]="Marca: ".$ar->Marca."; Material: ".$ar->Material."; Precio: ".$ar->Precio_Venta."; Codigo: ".$ar->Codigo;
+        }
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['compra/creates', 'id' => $id]);
+        }
+
+        return $this->render('createa', [
+            'model' => $model,
+            'aros'=>$aros,
+            'id'=>$id,
+        ]);
+    }
+
 
     /**
      * Updates an existing Detallecompra model.
